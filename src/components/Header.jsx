8b63b740fd33298fe36fae5ceddcf72d6bb8f0db
Krimothiazine <krimouv1995@gmail.com>
2023-09-05 22:17:27 +0100

    First style of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,35 @@ import Icon from "@mdi/react";
 
 const Header = () => {
   return (
-    <header className=" flex items-center justify-between px-20">
+    <header className=" flex items-center justify-between border-b-2 border-emerald-300 bg-neutral-900 px-20 text-neutral-200">
       <div className="flex items-center">
-        <Icon className="mr-3 inline w-8" path={mdiAccountSchoolOutline} />
+        <Icon
+          className="mr-3 inline w-8 text-emerald-300"
+          path={mdiAccountSchoolOutline}
+        />
         <h1 className="font-logo inline text-3xl font-bold">Discipulus</h1>
       </div>
       <nav className="space-x-4">
         <ul className="flex items-center justify-between gap-8">
           <li>
-            <a href="">Cours</a>
+            <a href="" className="hover:text-emerald-300">
+              Cours
+            </a>
           </li>
           <li>
-            <a href="">Extra</a>
+            <a href="" className="hover:text-emerald-300">
+              Extra
+            </a>
           </li>
           <li>
-            <a href="">Technologies</a>
+            <a href="" className="hover:text-emerald-300">
+              Technologies
+            </a>
           </li>
           <li>
-            <a href="">Contact</a>
+            <a href="" className="hover:text-emerald-300">
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
