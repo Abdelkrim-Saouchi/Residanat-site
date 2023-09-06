d3d20704b5a2f7ca925cef554e3c7d2e9abc3bc9
Krimothiazine <krimouv1995@gmail.com>
2023-09-06 10:34:04 +0100

    Create and style Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
 const Home = () => {
-  return <div></div>;
+  return (
+    <main className="flex min-h-screen justify-center bg-gray-900 px-20 text-neutral-200">
+      <div className="mt-20 flex max-w-3xl flex-col items-center gap-8">
+        <h2 className="text-center text-5xl font-bold">
+          Votre
+          <span className="text-green-400">
+            {" "}
+            Parcours vers la résidanat
+          </span>{" "}
+          commence ici
+        </h2>
+        <p className="w-2/3 text-center opacity-80">
+          Ce site web propose une vaste collection de cours en ligne destinés à
+          vous aider à réussir le concours de résidanat dans le domaine
+          pharmaceutique.
+        </p>
+        <a
+          className="rounded border border-neutral-200 px-6 py-2 font-bold hover:bg-gray-600"
+          href="#"
+        >
+          Voir les cours
+        </a>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
