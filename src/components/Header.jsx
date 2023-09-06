f1174af0b193b4b0a395dab8ceb5909f3de8b102
Krimothiazine <krimouv1995@gmail.com>
2023-09-06 09:36:32 +0100

    Change colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,33 +3,33 @@ import Icon from "@mdi/react";
 
 const Header = () => {
   return (
-    <header className=" flex items-center justify-between border-b-2 border-emerald-300 bg-neutral-900 px-20 text-neutral-200">
+    <header className=" flex items-center justify-between border-b-2 border-neutral-300 bg-gray-900 px-20 text-neutral-200">
       <div className="flex items-center">
         <Icon
-          className="mr-3 inline w-8 text-emerald-300"
+          className="mr-3 inline w-8 text-green-400"
           path={mdiAccountSchoolOutline}
         />
         <h1 className="font-logo inline text-3xl font-bold">Discipulus</h1>
       </div>
-      <nav className="space-x-4">
+      <nav className="mx-8">
         <ul className="flex items-center justify-between gap-8">
           <li>
-            <a href="" className="hover:text-emerald-300">
+            <a href="" className="hover:text-green-400">
               Cours
             </a>
           </li>
           <li>
-            <a href="" className="hover:text-emerald-300">
+            <a href="" className="hover:text-green-400">
               Extra
             </a>
           </li>
           <li>
-            <a href="" className="hover:text-emerald-300">
+            <a href="" className="hover:text-green-400">
               Technologies
             </a>
           </li>
           <li>
-            <a href="" className="hover:text-emerald-300">
+            <a href="" className="hover:text-green-400">
               Contact
             </a>
           </li>
