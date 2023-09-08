38ee14bc89459b75769fb56abe38b7475dca9d1d
Krimothiazine <krimouv1995@gmail.com>
2023-09-08 22:54:33 +0100

    Rename speciality title

--- src/pages/Pharmaceutical.jsx
+++ src/pages/Pharmaceutical.jsx
@@ -9,7 +9,7 @@ const Pharmaceutical = () => {
       "Ces disciplines forment l'essence même du métier de pharmacien ! La pharmacologie, la pharmacognosie, la toxicologie, et bien d'autres, sont des matières fascinantes qui confèrent au pharmacien sa singularité et sa polyvalence.",
     content: [
       {
-        specialty: "Pharmacie Galénique",
+        specialty: "Pharmacie Galénique et Industrielle",
         lessons: [
           {
             title:
