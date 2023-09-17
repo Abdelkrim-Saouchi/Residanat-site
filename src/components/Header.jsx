17c70c00f92d6f040656a36b2db1e59dfea6fa5a
Krimothiazine <krimouv1995@gmail.com>
2023-09-17 20:11:48 +0100

    Add better logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,11 @@
-import { mdiAccountSchoolOutline } from "@mdi/js";
-import Icon from "@mdi/react";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 const Header = () => {
   return (
     <header className=" flex items-center justify-between border-b border-gray-700 bg-gray-900 px-20 py-2 text-neutral-200">
       <Link className="flex items-center" to="/">
-        <Icon
-          className="mr-3 inline w-8 text-green-400"
-          path={mdiAccountSchoolOutline}
-        />
+        <img src={logo} alt="logo" className="mr-3 inline w-8 text-green-400" />
         <h1 className="inline font-logo text-3xl font-bold">Discipulus</h1>
       </Link>
       <nav className="mx-8">
