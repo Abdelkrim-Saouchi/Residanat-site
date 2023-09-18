ca5883d022b809cf1516b6811954657c3c0d21e8
Krimothiazine <krimouv1995@gmail.com>
2023-09-18 16:22:50 +0100

    Make path page responsive

--- src/components/LessonsCard.jsx
+++ src/components/LessonsCard.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const LessonsCard = ({ specialty, lessons }) => {
   return (
-    <div className="w-2/3 rounded-xl border border-gray-700 bg-gray-800">
+    <div className="w-full rounded-xl border border-gray-700 bg-gray-800 md:w-2/3">
       <h3 className="relative px-8 py-4 text-xl font-bold after:absolute after:bottom-0 after:left-0 after:block after:h-px after:w-full after:bg-gray-700">
         {specialty}
       </h3>
@@ -16,7 +16,9 @@ const LessonsCard = ({ specialty, lessons }) => {
             key={index}
             className="my-2 flex items-center gap-4 rounded-xl px-4 py-4 hover:bg-gray-600"
           >
-            <Icon path={mdiBookOpenVariant} size={1} className="opacity-75" />
+            <div>
+              <Icon path={mdiBookOpenVariant} size={1} className="opacity-75" />
+            </div>
             {lesson.title}
           </Link>
         ))}
--- src/components/PathLayout.jsx
+++ src/components/PathLayout.jsx
@@ -3,12 +3,12 @@ import LessonsCard from "./LessonsCard";
 
 const PathLayout = ({ svg, title, desc, content }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-6 bg-gray-900 px-20 py-10 text-neutral-200">
+    <main className="flex min-h-screen flex-col items-center gap-6 bg-gray-900 px-4 py-10 text-neutral-200 md:px-20">
       <div className="flex flex-col items-center gap-4">
         {svg}
-        <h2 className="text-3xl font-bold">{title}</h2>
+        <h2 className="text-center text-3xl font-bold">{title}</h2>
       </div>
-      <div className="w-2/3">
+      <div className="md:w-2/3">
         <div>
           <h3 className="mb-2 text-xl font-bold">Aperçu :</h3>
           <p className="opacity-80">{desc}</p>
