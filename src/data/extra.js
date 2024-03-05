522da4906f43aad4292c6e28e47a8148f7016da7
Krimothiazine <krimouv1995@gmail.com>
2024-03-05 20:22:04 +0100

    Update Extra links

--- src/data/extra.js
+++ src/data/extra.js
@@ -1,71 +1,83 @@
 export const extra = {
   links: [
     {
-      label: "1. Drive 2ème année 2022/2023",
+      label: "1. Drive 2ème année 2023/2024",
+      path: "https://drive.google.com/drive/folders/1pt0nx2NyvoOZcelXU12sI_btuqGPIxpm?fbclid=IwAR0lbL4SgMSVtiFzTfGoRE7oIUn_3k25HiwrIdz7ch_I8nmgNZcVuVQgqkA",
+    },
+    {
+      label: "2. Drive 2ème année 2022/2023",
       path: "https://drive.google.com/drive/folders/1dJANiY67wotrvNsE9IvwLoqKjP02e-me?fbclid=IwAR2ysj618vQidwJPr3AUdUYyU0T_APSQw7jeY4lZz01lqS1tNzga1LcK-Ic",
     },
     {
-      label: "2. Drive 2ème année 2021/2022",
+      label: "3. Drive 2ème année 2021/2022",
       path: "https://drive.google.com/drive/folders/1F7Uedl4XVqdQnGWf0YTEGCVjGFVV9bqI?fbclid=IwAR1TIsZcOXhJmrMS1ZLVrJsnSWRsVP04i3FgI3YXytUgfaZ29dJBj3M6FIk",
     },
     {
-      label: "3. Drive 3ème année 2022/2023",
+      label: "4. Drive 3ème année 2022/2023",
       path: "https://drive.google.com/drive/folders/1J7zPOorxGoS6UNuoxwzxc3J6EeQGTU5y?fbclid=IwAR0JELK4iV90SsdDtCrDTzMIK6gEwRCwTszfVljPpzyMXMRl2K-PtR5hpD0",
     },
     {
-      label: "4. Drive 3ème année 2021/2022",
+      label: "5. Drive 3ème année 2021/2022",
       path: "https://drive.google.com/drive/folders/1vJCm5RkyxjkXWyEpQ46A55ZKgCbpRPi9?fbclid=IwAR23QGrHGkBiXlWxakPCKiUWDqsb2LbJzuX5q74a3Hhp4jHw1wY9q17Q1rM",
     },
     {
-      label: "5. Drive 4ème année 2022/2023",
+      label: "6. Drive 4ème année 2023/2024",
+      path: "https://drive.google.com/drive/folders/1xBHIhal9gXrcdb7W7iVkbpttep2Xy1W3?fbclid=IwAR1gO8QmkAA2NPeVvK-p_fPT7NkbCnV4aDAXxAPYgsty8KDXt5xsO2BX5go",
+    },
+    {
+      label: "7. Drive 4ème année 2022/2023",
       path: "https://drive.google.com/drive/folders/1-66NXDyp13Oc9JvLq03rDEsmT6Oe0zDp?fbclid=IwAR1tLAV3bo2kFwhDDzYpCje-31dZjXG61t5w0eKreFi6hf-8HVDdT1lvpo0",
     },
     {
-      label: "6. Drive 4ème année 2021/2022",
+      label: "8. Drive 4ème année 2021/2022",
       path: "https://drive.google.com/drive/u/0/folders/1XdZ7wt2BUloI5FVbIsLKl7GIRZzyyaUd?fbclid=IwAR0aZuUqOAzCzeQFofUhAuP0wES19v1DlBxLY167rJoKSOtu9SNWSnubzlI",
     },
     {
-      label: "7. Drive 5ème année 2022/2023",
+      label: "9. Drive 5ème année 2023/2024",
+      path: "https://drive.google.com/drive/folders/11cAEbF-Rei334EKVzZedTzS4_yDd8F9b?fbclid=IwAR02UE35NEOP2hPS3vAuy7cERola-8pIr5U5eFh2H9pTkcpOnAk0o8t4Us8",
+    },
+    {
+      label: "10. Drive 5ème année 2022/2023",
       path: "https://drive.google.com/drive/folders/1PAn-0IWbdjNMqFQCYlTbqSmF553OSkqJ?fbclid=IwAR07iZYGyL-FzZHS2ON1GxGbza08wJfbWA8IVmQNvTiaKyXcR-c-BWcd-Zc",
     },
     {
-      label: "8. Drives de l'année 2021/2022",
+      label: "11. Drives de l'année 2021/2022",
       path: "https://www.facebook.com/permalink.php?story_fbid=pfbid02fSxsdYEvEnYmNwS32iyD8PsEf1kdQqi8zMq6bQDNz3eiFHRS5Lozpo3P4zE1zNJ8l&id=102281815820234",
     },
     {
-      label: "9. QCM, EMDS et Résumés",
+      label: "12. QCM, EMDS et Résumés",
       path: "https://drive.google.com/drive/folders/1dDwAAuxzjUL8KV2wQwoBSy8CN9wtWKLk?fbclid=IwAR353ng2RJIPCn2F9hbEaL3iw1yhKSsH7vNS9kIQ5XQxGl4917P3x4KGE9s",
     },
     {
-      label: "10. Autre EMDs",
+      label: "13. Autre EMDs",
       path: "https://drive.google.com/drive/folders/174eTI_cTX0039RDWZeEeFvQQwKbSUdks?fbclid=IwAR2fQ-d_nc9L7TbwFeIGAKXfV1dMIspaCyQveUpUqsc9yXGSretwxBIWSZE",
     },
     {
-      label: "11. Sujets Résidanats",
+      label: "14. Sujets Résidanats",
       path: "https://drive.google.com/drive/folders/1ulS2ps7WkZ371Wme6s0l6sti-7rzseRt?fbclid=IwAR1cCyGy3KM1S41l706-ZQQ3bhBCOcKFKAHxFCLUP11QyFIuhQF1W75Ti70",
     },
     {
-      label: "12. Site officiel de département pharmacie constantine",
+      label: "15. Site officiel de département pharmacie constantine",
       path: "https://facmed.univ-constantine3.dz/?page_id=1922",
     },
     {
-      label: "13. Chaine youtube en parasitologie",
+      label: "16. Chaine youtube en parasitologie",
       path: "https://www.youtube.com/playlist?list=PLsjxu-h3dab1dIQJZNmjdHZG79f1kONRw",
     },
     {
-      label: "14. Autre site de cours: residanat25.epizy.com ",
+      label: "17. Autre site de cours: residanat25.epizy.com ",
       path: "https://www.residanat25.epizy.com/",
     },
     {
-      label: "15. Autre site : thephamacistdz.com",
+      label: "18. Autre site : thephamacistdz.com",
       path: "https://thephamacistdz.com/",
     },
     {
-      label: "16. Autre site: pharma Study dz",
+      label: "19. Autre site: pharma Study dz",
       path: "https://sites.google.com/view/pharmastudy-dz/",
     },
     {
-      label: "17. Le site national des livres universitaires: Iqraa",
+      label: "20. Le site national des livres universitaires: Iqraa",
       path: "https://st.iqraa.opu.dz/",
     },
   ],
